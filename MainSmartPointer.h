1095c2b8f50e7e9d9b6041bd73d80412b1f902e7
dreamIIx <shiba.zheka65@gmail.com>
2018-04-01 00:37:58 +0500

    SmartPointer

---
+++ MainSmartPointer.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+
+namespace drm
+{
+	template <typename T>
+	class SmartPointer
+	{
+	public:
+
+		SmartPointer(T *pt)
+			: ptr(pt)
+		{}
+
+		~SmartPointer()
+		{
+			delete ptr;
+		}
+
+		T& operator*()
+		{
+			return *ptr;
+		}
+
+	private:
+		T *ptr;
+	};
+};
